b815cbaf1dd46b72af9ab5c5ac1aeb0048f8b99f
Ahmed El-Kholy <a.adel27111@gmail.com>
2024-12-22 11:38:05 +0200

    Q19

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -6,6 +6,33 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
+            #region Q19 Write a program that prints an identity matrix using for loop, in other words takes a value n from the user and shows the identity table of size n * n.   
+            //for (; ; )
+            //{
+            //    Boolean Flag = int.TryParse(Console.ReadLine(), out int N);
+            //    if (Flag & N != 0)
+            //    {
+            //        int[,] IdentityMat = new int[N, N];
+            //        for (int i = 0; i < IdentityMat.GetLength(0); i++)
+            //        {
+            //            for (int j = 0; j < IdentityMat.GetLength(1); j++)
+            //            {
+            //                if (i == j)
+            //                {
+            //                    IdentityMat[i, j] = 1;
+            //                }
+            //                Console.Write($"{IdentityMat[i, j]} ");
+            //            }
+            //            Console.WriteLine();
+            //        }
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Enter Number Greater than Zero");
+            //    }
+            //}
+            #endregion
 
         }
 
